1850b6ff107d481042496227db56daeae573fb87
RithvikKolluru <rithwikkolluru7@gmail.com>
2026-09-18 09:10:45 +0530

    Implement CRC-CCITT error detection code

---
+++ 02-CRC-Polynomials/crc_ccitt.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char data[100], divisor[] = "10001000000100001", temp[100];
+    int dataLen = 0, divLen = strlen(divisor);
+
+    printf("Enter Data Bits: ");
+    scanf("%s", data);
+
+    dataLen = strlen(data);
+    strcpy(temp, data);
+
+    for(int i = 0; i < divLen - 1; i++)
+        temp[dataLen + i] = '0';
+
+    temp[dataLen + divLen - 1] = '\0';
+
+    for(int i = 0; i < dataLen; i++) {
+        if(temp[i] == '1') {
+            for(int j = 0; j < divLen; j++)
+                temp[i+j] = (temp[i+j] == divisor[j]) ? '0' : '1';
+        }
+    }
+
+    printf("CRC-CCITT Code: ");
+    for(int i = dataLen; i < dataLen + divLen - 1; i++)
+        printf("%c", temp[i]);
+
+    printf("\n");
+
+    return 0;
+}
